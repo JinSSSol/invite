3ceee98739084f1b279ac915b0d48fff20f9226c
JinSol <wlsthf0003@naver.com>
2023-06-25 01:09:29 +0900

    feat : [005-EMBEDDED_REDIS] 임베디드 레디스로 변경

--- api/src/main/java/com/study/api/ApiApplication.java
+++ api/src/main/java/com/study/api/ApiApplication.java
@@ -3,6 +3,7 @@ package com.study.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.study.domain"})
 @EntityScan(basePackages = {"com.study.domain"})
+@ComponentScan(basePackages = {"com.study.domain", "com.study.api"})
 public class ApiApplication {
 
     public static void main(String[] args) {
---
+++ domain/src/main/java/com/study/domain/config/EmbeddedRedisConfig.java
@@ -0,0 +1,29 @@
+package com.study.domain.config;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+@Configuration
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.embedded-redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
